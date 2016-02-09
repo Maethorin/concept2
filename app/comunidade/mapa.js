bc17604a4d4e72c3308cd72677e2a840fa7e137c
Giovanna <giovanna14batalha@gmail.com>
2016-02-09 10:03:27 -0200

    campo de busca pagina comunidade

--- app/comunidade/mapa.js
+++ app/comunidade/mapa.js
@@ -1,16 +1,14 @@
 var map;
-function initMap() {
+function initAutocomplete() {
     var mylatlng = {lat: -14.235004, lng: -51.92528};
-    map = new google.maps.Map(document.getElementById('mapa'), {
+   var map = new google.maps.Map(document.getElementById('mapa'), {
         center: mylatlng,
         zoom: 4,
         zoomControl: false,
         mapTypeControl: false,
-        streetViewControl: false
+        streetViewControl: false,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
     });
-
-
-
 
     var image = 'static/concept2-amarelo.png';
        function carregarPontos(){
@@ -21,14 +19,61 @@ function initMap() {
                        title:"pontos!!",
                        map:map,
                        icon: ponto.icone
+
                    })
                });
             });
 
+
        }
      carregarPontos();
-}
 
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+     var markers = [];
+     searchBox.addListener('places_changed', function() {
+         var places = searchBox.getPlaces();
+
+         if (places.length == 0) {
+             return;
+         }
+         markers.forEach(function(marker) {
+      marker.setMap();
+      });
+         markers = [];
+     var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+        };
+    markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+ if (place.geometry.viewport) {
+     bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+
+
+
+
+})
+    }
 
 
 
