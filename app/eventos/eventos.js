6f0aaae5bb6598b1d0934184a57aaef1c076c8d5
giovanna <giovanna14batalha@gmail.com>
2016-02-17 17:19:15 -0200

    terminando formulario eventos cabra-ri

--- app/eventos/eventos.js
+++ app/eventos/eventos.js
@@ -1,36 +1,47 @@
-
 'use strict';
 angular.module('concept.eventos', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider) {
-      $routeProvider
-          .when('/eventos', {
-            templateUrl: 'eventos/eventos.html',
-            controller: 'EventosController'
-          })
-          .when('/eventos/cabra', {
-            templateUrl: 'eventos/cabra-ri.html',
-            controller: 'CampoController'
-          })
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/eventos', {
+                templateUrl: 'eventos/eventos.html',
+                controller: 'EventosController'
+            })
+            .when('/eventos/cabra', {
+                templateUrl: 'eventos/cabra-ri.html',
+                controller: 'CampoController'
+            })
     }])
-    .controller('EventosController', ['$rootScope','$scope', '$http', function($rootScope,$scope, $http) {
+    .controller('EventosController', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
         $rootScope.cssPagina = "pagina-eventos";
         $rootScope.titulo = "Eventos";
         $scope.eventos = []
-        $http.get('/eventos/eventos.json').then(function(response){
-        $scope.eventos = response.data;
+        $http.get('/eventos/eventos.json').then(function (response) {
+            $scope.eventos = response.data;
         });
     }])
-    .controller('CampoController',['$scope', function($scope) {
+    .controller('CampoController', ['$scope', function ($scope) {
         $scope.dadosInscricao = {
             "nome": null,
             "sobrenome": null,
-             "afiliacao": null,
+            "afiliacao": null,
             "cpf": null,
-             "telefone": null,
+            "telefone": null,
             "time": null,
-             "data": null,
+            "data": null,
             "email": null
             //adicionar os outros campos
         };
-        $scope.maskDef = {'maskDefinitions': { '9': /\d/, 'D': /[0-3]/, 'd':/[0-1]/, 'M': /[0-1]/, 'm': /[0-2]/}};
+        $scope.maskDef = {'maskDefinitions': {'9': /\d/, 'D': /[0-3]/, 'd': /[0-1]/, 'M': /[0-1]/, 'm': /[0-2]/}};
+        $scope.campoEstaValido = function (campo) {
+            return campo.$touched && !campo.$error.required
+
+        };
+        $scope.campoEstaInvalido = function (campo) {
+            return campo.$touched && campo.$error.required
+
+        } ;
+        $scope.campoNaoTocado = function (campo) {
+            return !campo.$touched
+
+        };
     }]);
