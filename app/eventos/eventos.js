df1b4625224672805f582c9b966d4b45386db966
giovanna <giovanna14batalha@gmail.com>
2016-02-17 00:40:14 -0200

    formulario

--- app/eventos/eventos.js
+++ app/eventos/eventos.js
@@ -7,12 +7,30 @@ angular.module('concept.eventos', ['ngRoute'])
             templateUrl: 'eventos/eventos.html',
             controller: 'EventosController'
           })
+          .when('/eventos/cabra', {
+            templateUrl: 'eventos/cabra-ri.html',
+            controller: 'CampoController'
+          })
     }])
- .controller('EventosController', ['$rootScope','$scope', '$http', function($rootScope,$scope, $http) {
+    .controller('EventosController', ['$rootScope','$scope', '$http', function($rootScope,$scope, $http) {
         $rootScope.cssPagina = "pagina-eventos";
         $rootScope.titulo = "Eventos";
         $scope.eventos = []
         $http.get('/eventos/eventos.json').then(function(response){
         $scope.eventos = response.data;
         });
+    }])
+    .controller('CampoController',['$scope', function($scope) {
+        $scope.dadosInscricao = {
+            "nome": null,
+            "sobrenome": null,
+             "afiliacao": null,
+            "cpf": null,
+             "telefone": null,
+            "time": null,
+             "data": null,
+            "email": null
+            //adicionar os outros campos
+        };
+        $scope.maskDef = {'maskDefinitions': { '9': /\d/, 'D': /[0-3]/, 'd':/[0-1]/, 'M': /[0-1]/, 'm': /[0-2]/}};
     }]);
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -2,6 +2,7 @@
 
 angular.module('concept', [
     'ngRoute',
+    'ui.mask',
     'concept.home',
     'concept.produtos',
     'concept.noticias',
@@ -10,29 +11,3 @@ angular.module('concept', [
     'concept.suporte',
     'concept.contato'
 ]);
-
-app.run(['$rootscope', '$window', 'srvAuth',
-    function ($rootscope, $window, sAuth) {
-        $rootscope.user = {};
-        $window.fbAsyncInit = function () {
-            FB.init({
-                appId: '1745932382309577',
-                status: true,
-                cookie: true,
-                xfbml: true,
-                version: 'v2.5'
-            });
-            sAuth.watchAuthenticationStatusChange();
-        };
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {
-                return;
-            }
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "//connect.facebook.net/pt_BR/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    }
-])
