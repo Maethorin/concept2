2c099eb5ce5b426e15a044b3581196afb058baf6
maethorin <maethorin@gmail.com>
2016-02-25 11:26:39 -0300

    Adicionando Python

--- app/api.py
+++ app/api.py
@@ -6,6 +6,4 @@ import resources
 
 def create_api(app):
     api = Api(app)
-    api.add_resource(resources.Musicas, '/api/musicas', '/api/musicas/<int:musica_id>')
-    api.add_resource(resources.ParserMusica, '/api/letra-musica')
-    api.add_resource(resources.EditorMusica, '/api/editor-musica', '/api/editor-musica/<int:musica_id>')
+    api.add_resource(resources.Produtos, '/api/produtos', '/api/produtos/<int:produto_id>')
