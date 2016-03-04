959accf5a1b102cd1c3e72ae4992c10f53562f24
giovanna <giovanna14batalha@gmail.com>
2016-03-04 18:05:30 -0300

    pagina home

--- app/static/js/app/produtos/produtos.js
+++ app/static/js/app/produtos/produtos.js
@@ -6,8 +6,17 @@ angular.module('concept2.produtos', ['ngRoute'])
             templateUrl: '/angular/produtos.html',
             controller: 'ProdutosController'
           })
+          .when('/produtos/modeloD', {
+              templateUrl: '/angular/produtos/modelod.html',
+              controller: 'ModeloController'
+          })
     }])
  .controller('ProdutosController', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
         $rootScope.pagina = "produtos";
         $rootScope.titulo = "Produtos"
-    }]);
+    }])
+.controller('ModeloController', function($routeParams, $rootScope, $scope, $http){
+     $scope.slug = $routeParams.slug;
+     $rootScope.pagina = "produtos";
+     $rootScope.titulo = "Produtos";
+})
