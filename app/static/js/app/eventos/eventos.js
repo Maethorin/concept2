e2cab406928d6decb77cecd8f89cac1c458f41e4
maethorin <maethorin@gmail.com>
2016-03-08 00:21:54 -0300

    Corrigindo mascara de data

--- app/static/js/app/eventos/eventos.js
+++ app/static/js/app/eventos/eventos.js
@@ -87,6 +87,7 @@ angular.module('concept2.eventos', ['ngRoute'])
         $scope.itemMenu = $routeParams.itemMenu || 'sobre';
         $scope.template = '/angular/evento/{0}.html'.format([$scope.itemMenu]);
         $scope.urlLojaInscricao = 'https://concept2.com.br/shop/index.php?route=product/product&product_id=125';
+        $scope.maskDef = {'maskDefinitions': {'9': /\d/, 'D': /[0-3]/, 'd': /[0-9]/, 'M': /[0-1]/, 'm': /[0-2]/}};
         $rootScope.pagina = "eventos";
         $scope.itensMenu = [
             {"slug": "sobre", "nome": "Sobre"},
@@ -154,7 +155,6 @@ angular.module('concept2.eventos', ['ngRoute'])
 
             if ($scope.itemMenu == 'inscricao') {
                 $scope.mensagemErro = '';
-                $scope.maskDef = {'maskDefinitions': {'9': /\d/, 'D': /[0-3]/, 'd': /[0-9]/, 'M': /[0-1]/, 'm': /[0-2]/}};
                 $scope.tiposAfiliacoes = [
                     {'codigo': 'clube', 'label': 'Clube'},
                     {'codigo': 'academia', 'label': 'Academia'},
