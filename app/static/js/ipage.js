2a107198b8bad7b59482508ca9155cf60cd29fc9
maethorin <maethorin@gmail.com>
2016-03-10 04:26:27 -0300

    Problema no login

--- app/static/js/ipage.js
+++ app/static/js/ipage.js
@@ -40,8 +40,8 @@ Date.prototype.nomeDia = function() {
     return nomesDias[this.getDay()];
 };
 
-//var urlBackEnd = 'https://concept2-staging.herokuapp.com';
-var urlBackEnd = 'http://localhost:5000';
+var urlBackEnd = 'https://concept2-staging.herokuapp.com';
+//var urlBackEnd = 'http://localhost:5000';
 
 angular.module('concept2', [
     'ngRoute',
@@ -58,8 +58,7 @@ angular.module('concept2', [
             'self',
             '{0}/**'.format([urlBackEnd])
         ]);
-        $routeProvider
-          .when('/', {redirectTo: '/eventos/cabra-ri'});
+        $routeProvider.when('/', {redirectTo: '/eventos/cabra-ri'});
         $httpProvider.defaults.withCredentials = true;
     })
     .run(function ($rootScope, Autentic) {
