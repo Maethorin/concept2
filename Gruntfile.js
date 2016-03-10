1ecf6428c5b656515e0ba58afb66d494a363dd62
maethorin <maethorin@gmail.com>
2016-03-10 02:25:29 -0300

    Tentando rodar no heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,4 +65,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.registerTask('default', ['concat', 'uglify', 'watch', 'compass']);
+    grunt.registerTask('heroku', ['concat:app', 'uglify:app']);
 };
