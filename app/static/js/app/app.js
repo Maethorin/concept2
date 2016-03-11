6e4faa8fcb13e2696a6aae23d3211afaa6673320
maethorin <maethorin@gmail.com>
2016-03-11 20:56:32 -0300

    Corrigindo a url do backend

--- app/static/js/app/app.js
+++ app/static/js/app/app.js
@@ -35,8 +35,8 @@ Date.prototype.idadeNascidoEm = function(nascimento) {
     return nomesDias[this.getDay()];
 };
 
-//var urlBackEnd = 'https://concept2-staging.herokuapp.com';
-var urlBackEnd = 'http://localhost:5000';
+var urlBackEnd = 'https://concept2-staging.herokuapp.com';
+//var urlBackEnd = 'http://localhost:5000';
 
 angular.module(
     'concept2',
