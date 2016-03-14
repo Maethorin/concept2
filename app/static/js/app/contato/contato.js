34b03fd77e8f817a655fe4e06e0380c068d28855
giovanna <giovanna14batalha@gmail.com>
2016-03-14 18:44:23 -0300

    pagina contato pt2

--- app/static/js/app/contato/contato.js
+++ app/static/js/app/contato/contato.js
@@ -7,7 +7,16 @@ angular.module('concept2.contato', ['ngRoute'])
                 controller: 'ContatoController'
             })
     }])
+     .config(['uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider) {
+        uiGmapGoogleMapApiProvider.configure({
+            key: 'AIzaSyDzcLVVah4PjogAqerQdBcYowwzJcsKjv0',
+            v: '3.20',
+            language: 'pt-br',
+            libraries: 'places'
+        });
+    }])
     .controller('ContatoController', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
         $rootScope.pagina = "contato";
-        $rootScope.titulo = "Contato"
+        $rootScope.titulo = "Contato";
+        $scope.mapa = { center: {latitude: -22.9747013, longitude: -43.3734641}, zoom: 17 };
     }]);
