f17c2c9b0f1f3cb35b39a81d5b9f548b8776d887
maethorin <maethorin@gmail.com>
2016-03-21 22:32:52 -0300

    https na url da loja

--- dados/cabra-ri-006.sql
+++ dados/cabra-ri-006.sql
@@ -3,6 +3,6 @@ UPDATE public.eventos SET
   url_pagamento_cursos = 'https://concept2.com.br/shop/index.php?route=product/product&product_id=170'
 WHERE slug = 'cabra-ri';
 
-UPDATE public.cursos SET valor = 40000, url_pagamento = 'http://concept2.com.br/shop/index.php?route=product/product&product_id=127' WHERE nome = 'CURSO Introdutório de Remo Indoor';
+UPDATE public.cursos SET valor = 40000, url_pagamento = 'https://concept2.com.br/shop/index.php?route=product/product&product_id=127' WHERE nome = 'CURSO Introdutório de Remo Indoor';
 
-UPDATE public.cursos SET valor = 40000, url_pagamento = 'http://concept2.com.br/shop/index.php?route=product/product&product_id=169' WHERE nome = 'CURSO de Especialização';
+UPDATE public.cursos SET valor = 40000, url_pagamento = 'https://concept2.com.br/shop/index.php?route=product/product&product_id=169' WHERE nome = 'CURSO de Especialização';
