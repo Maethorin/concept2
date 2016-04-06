93d1ea6b30737d0cfc1e032ee494af930f23c8c2
giovanna <giovanna14batalha@gmail.com>
2016-04-05 21:00:43 -0300

    pagina suporte- manutencao

--- app/static/js/app/suporte/suporte.js
+++ app/static/js/app/suporte/suporte.js
@@ -11,6 +11,10 @@ angular.module('concept2.suporte', ['ngRoute'])
            templateUrl: '{0}/angular/suporte/download.html'.format([urlBackEnd]),
            controller: 'DownloadController'
         })
+          .when('/suporte/:slug', {
+              templateUrl: '{0}/angular/suporte/suportes.html'.format([urlBackEnd]),
+              controller: 'SuportesController'
+          })
 
     }])
  .controller('SuporteController', ['$rootScope','$scope', '$http', function($rootScope, $scope, $http) {
@@ -24,4 +28,15 @@ angular.module('concept2.suporte', ['ngRoute'])
          $scope.trataHtml = function(html) {
             return $sce.trustAsHtml(html);
         };
+    }])
+.controller('SuportesController', ['$routeParams', '$rootScope', '$scope', '$sce', function($routeParams, $rootScope, $scope, $sce){
+         $scope.slug = $routeParams.slug;
+         $scope.suportetemplate = '{0}/angular/suporte/{1}/suportes.html'.format([urlBackEnd, $scope.slug]);
+         $rootScope.pagina = "suporte";
+         $scope.trataHtml = function(html) {
+            return $sce.trustAsHtml(html);
+        };
     }]);
+$('.carousel').carousel({
+    interval:false
+});
